cf2af8993568bd909d76f7854b6730c0f3bc4ef1
Oskari Koskinen <oskari.koskinen@helsinki.fi>
2018-02-08 14:05:16 +0200

    viesti

--- info.js
+++ info.js
@@ -93,7 +93,7 @@ app.get('/persons/:id', (req, res) => {
 // Tähän tulee morgan-funktio
 // tiny config => :method :url :status :res[content-length] - :response-time ms
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
