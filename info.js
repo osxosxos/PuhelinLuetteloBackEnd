2e33e2337eb59fe5a1dbc4a31bf1b2524f4c5e34
Oskari Koskinen <oskari.koskinen@helsinki.fi>
2018-02-08 14:58:57 +0200

    portti fixi

--- info.js
+++ info.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
+app.use(express.static('build'))
 app.use(bodyParser.json())
 
 let persons = [
